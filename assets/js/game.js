f72e31594abb0806556f022295c9de194ffd4411
Hogtown13 <tribenation13@yahoo.com>
2022-02-27 14:21:05 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,9 +69,52 @@ var fight = function(enemyName) {
     };
   }
 }
-for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(enemyNames[i]);
 
-}
+// function to start a new game
+var startGame = function () {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+      
+      var pickedEnemyName = enemyNames[i];
+    
+      enemyHealth = 50;
+    
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  //Play again
+  endGame();
+};
+
+
+var endGame = function() {
+  //if player is still alive, player wins!
+  if(playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + "." );
+  }
+  else {
+    window.alert("You've lost your robot in battle.")
+  }
+  window.alert("The game has now ended. Let's see how you did!");
+  
+  var playAgainConfirm = window.confirm("Would you like to play again?") 
+
+  if(playAgainConfirm) {
+    //restart game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+};
+startGame();
